a2c38539030aff9ad795d7a645901fbeca748f00
Mirry254 <mwongelimirriam01@gmail,com>
2025-05-03 15:44:50 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pet import Pet
+
+# Create a pet
+my_pet = Pet("Buddy")
+
+# Try out some actions
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("roll over")
+my_pet.show_tricks()
+my_pet.get_status()
